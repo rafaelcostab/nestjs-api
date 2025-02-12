58a7791910556bcf0b38d340e455ae2ac0c71369
Rafael Costa <rafael.costab@hotmail.com>
2025-02-12 08:09:25 -0300

    feat: popula o banco com dados para teste
    no start da aplicação populamos o banco com dados para efetuar testes.

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,11 +1,32 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   imports: [PrismaModule, ProductsModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
-export class ProductsModule {}
+export class ProductsModule implements OnModuleInit {
+  constructor(
+    private prismaService: PrismaService,
+    private productService: ProductsService,
+  ) {}
+
+  async onModuleInit() {
+    const products = new Array(10).fill(0).map((_, index) => index + 1);
+
+    await this.prismaService.product.deleteMany();
+
+    for (const productIndex of products) {
+      await this.productService.create({
+        name: `Product ${productIndex}`,
+        slug: `product-${productIndex}`,
+        description: `Description of product ${productIndex}`,
+        price: productIndex * 100,
+      });
+    }
+  }
+}
